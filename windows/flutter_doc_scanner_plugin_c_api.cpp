8c6651c008ddc6a973168e5518d917edd2cae753
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2025-01-17 13:13:37 +0530

    Updated cmakelist

--- windows/flutter_doc_scanner_plugin_c_api.cpp
+++ windows/flutter_doc_scanner_plugin_c_api.cpp
@@ -1,10 +1,10 @@
 #include "include/flutter_doc_scanner/flutter_doc_scanner_plugin_c_api.h"
 
 #include <flutter/plugin_registrar_windows.h>
+
 #include "flutter_doc_scanner_plugin.h"
 
-// This function bridges the C API to the C++ implementation of the plugin.
-void FlutterDocScannerPluginRegisterWithRegistrar(
+void FlutterDocScannerPluginCApiRegisterWithRegistrar(
     FlutterDesktopPluginRegistrarRef registrar) {
   flutter_doc_scanner::FlutterDocScannerPlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
