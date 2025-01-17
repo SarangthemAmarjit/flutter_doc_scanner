571f18b1a3789141946c6c95e79b8a6b48d5c645
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2025-01-17 14:10:01 +0530

    Updated cmakelist

--- windows/flutter_doc_scanner_plugin.h
+++ windows/flutter_doc_scanner_plugin.h
@@ -3,26 +3,51 @@
 
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
+#pragma once
 
 #include <memory>
 
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
+#else
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllimport)
+#endif
+
 namespace flutter_doc_scanner {
 
-class FlutterDocScannerPlugin : public flutter::Plugin {
+/**
+ * The FlutterDocScannerPlugin class provides a plugin implementation
+ * for communication between Dart and native Windows code.
+ */
+class FLUTTER_PLUGIN_EXPORT FlutterDocScannerPlugin : public flutter::Plugin {
  public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+  /**
+   * Registers the plugin with the provided Windows plugin registrar.
+   * @param registrar The registrar that manages the plugin.
+   */
+  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
 
+  /**
+   * Constructor for FlutterDocScannerPlugin.
+   */
   FlutterDocScannerPlugin();
 
+  /**
+   * Destructor for FlutterDocScannerPlugin.
+   */
   virtual ~FlutterDocScannerPlugin();
 
-  // Disallow copy and assign.
+  // Prevent copying and assignment.
   FlutterDocScannerPlugin(const FlutterDocScannerPlugin&) = delete;
   FlutterDocScannerPlugin& operator=(const FlutterDocScannerPlugin&) = delete;
 
-  // Called when a method is called on this plugin's channel from Dart.
+  /**
+   * Handles method calls from the Dart side.
+   * @param method_call The method call object containing method name and arguments.
+   * @param result The result object for sending responses back to Dart.
+   */
   void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      const flutter::MethodCall<flutter::EncodableValue>& method_call,
       std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 };
 
